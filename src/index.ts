5f0c48765065f2c1db96971acc9a5a2bbf60d146
shidoshi93 <dann.ribeiroo@gmail.com>
2021-05-17 18:00:38 +0100

    refatorado cors.

--- src/index.ts
+++ src/index.ts
@@ -1,23 +1,13 @@
-import dotenv from "dotenv";
 import { AddressInfo } from "net";
 import express from "express";
 import { userRouter } from "./routes/userRouter";
 import { musicRouter } from "./routes/MusicRouter";
 import cors from "cors";
 
-
-
-dotenv.config();
 const app = express();
-
-/* const allowedOrigins = ['http://localhost:3003'];
-
-const options: cors.CorsOptions = {
-  origin: allowedOrigins
-}; */
-
-app.use(cors())
 app.use(express.json());
+app.use(cors())
+
 app.use("/user", userRouter);
 app.use("/music", musicRouter)
 
