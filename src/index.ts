905159339779c6915afeecaa51c5a5d4ef5892d4
shidoshi93 <dann.ribeiroo@gmail.com>
2021-05-17 03:35:48 +0100

    tentado fazer o cors funcionar

--- src/index.ts
+++ src/index.ts
@@ -7,7 +7,7 @@ import cors from "cors";
 
 dotenv.config();
 const app = express();
-app.use(cors({ origin: true }))
+app.use(cors())
 app.use(express.json());
 app.use("/user", userRouter);
 app.use("/music", musicRouter)
