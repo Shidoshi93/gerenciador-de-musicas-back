c0db096b46e684c2ced288d247cc95a91a81bde5
shidoshi93 <dann.ribeiroo@gmail.com>
2021-05-17 03:13:01 +0100

    Reconfigurando cors

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
 import dotenv from "dotenv";
-import {AddressInfo} from "net";
+import { AddressInfo } from "net";
 import express from "express";
 import { userRouter } from "./routes/userRouter";
 import { musicRouter } from "./routes/MusicRouter";
@@ -7,17 +7,16 @@ import cors from "cors";
 
 dotenv.config();
 const app = express();
-
+app.use(cors({ origin: true }))
 app.use(express.json());
-app.use(cors({origin: true}))
 app.use("/user", userRouter);
 app.use("/music", musicRouter)
 
 const server = app.listen(3003, () => {
-    if (server) {
-      const address = server.address() as AddressInfo;
-      console.log(`Servidor rodando em http://localhost:${address.port}`);
-    } else {
-      console.error(`Falha ao rodar o servidor.`);
-    }
-  });
+  if (server) {
+    const address = server.address() as AddressInfo;
+    console.log(`Servidor rodando em http://localhost:${address.port}`);
+  } else {
+    console.error(`Falha ao rodar o servidor.`);
+  }
+});
