3e636286df87642bd48c8b007614a4dd09cc42f0
shidoshi93 <dann.ribeiroo@gmail.com>
2021-05-17 03:52:34 +0100

    tetando passar options para o cors.

--- src/index.ts
+++ src/index.ts
@@ -7,7 +7,14 @@ import cors from "cors";
 
 dotenv.config();
 const app = express();
-app.use(cors({ origin: true }))
+
+const allowedOrigins = ['http://localhost:3000'];
+
+const options: cors.CorsOptions = {
+  origin: allowedOrigins
+};
+
+app.use(cors(options))
 app.use(express.json());
 app.use("/user", userRouter);
 app.use("/music", musicRouter)
